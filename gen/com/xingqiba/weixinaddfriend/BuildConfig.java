9b15cc804b2b9be511370bbdeebfd4228995c4c9
unknown <545381409@qq.com>
2016-08-04 14:11:58 +0800

    a

--- gen/com/xingqiba/weixinaddfriend/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.xingqiba.weixinaddfriend;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/xingqiba/weixinaddfriend/aidl/ProcessConnection.aidl
+++
@@ -1,4 +0,0 @@
-package com.xingqiba.weixinaddfriend.aidl;
-interface ProcessConnection{
-	String getProcessName();
-}
